5a8ea19f2788ba2cc48ca01dc4497f42fc0cc1ad
chk121 <hekouck@163.com>
2019-07-20 20:33:25 +0800

    Submission 1st version

--- src/route_model.h
+++ src/route_model.h
@@ -1,18 +1,36 @@
-#pragma once
+#ifndef ROUTE_MODEL_H
+#define ROUTE_MODEL_H
 
 #include <limits>
 #include <cmath>
 #include <unordered_map>
 #include "model.h"
 #include <iostream>
+#include <math.h>
+#include <vector>
+
+
+using std::unordered_map;
+using std::vector;
+
 
 class RouteModel : public Model {
-
   public:
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
-        
+      	Node * parent = nullptr;
+      	float h_value = std::numeric_limits<float>::max();
+      	float g_value = 0.0;
+      	bool visited = false;
+      	vector<Node *> neighbors;
+      
+      	float distance(Node other) const{
+          float e_dist;
+          e_dist = sqrt(pow((x - other.x), 2)+pow((y - other.y), 2));
+          return e_dist;
+        }
+      	void FindNeighbors();
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
       
@@ -20,13 +38,27 @@ class RouteModel : public Model {
         // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
+      	Node * FindNeighbor(vector<int> node_indices);
     };
     
     // Add public RouteModel variables and methods here.
-    RouteModel(const std::vector<std::byte> &xml);  
+    RouteModel(const std::vector<std::byte> &xml);
+  	auto &SNodes() { return m_Nodes;}
+  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+  
+  	auto &GetNodeToRoadMap(){
+      return node_to_road;
+    }
+  
+  	Node &FindClosestNode(float x, float y);
 
   private:
     // Add private RouteModel variables and methods here.
+  	vector<Node> m_Nodes;
+  	unordered_map <int, vector<const Model::Road*>> node_to_road;
+  	void CreateNodeToRoadHashmap();
 
 };
+
+#endif
--- src/route_planner.h
+++ src/route_planner.h
@@ -1,17 +1,33 @@
-#pragma once
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
 
 #include <iostream>
 #include <vector>
 #include <string>
 #include "route_model.h"
 
+using std::vector;
 
 class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
+  	float GetDistance(){
+      return distance;
+    }
+  	void AStarSearch();
 
   private:
     // Add private variables or methods declarations here.
     RouteModel &m_Model;
+  	RouteModel::Node *start_node;
+  	RouteModel::Node *end_node;
+  	vector<RouteModel::Node*> open_list;
+  	float distance;
+  	RouteModel::Node *NextNode();
+  	void AddNeighbors(RouteModel::Node *current_node);
+  	vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+  	float CalculateHValue(const RouteModel::Node* node);
 };
+
+#endif
